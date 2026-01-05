4f7c8c0cdf976c74753eb581409254cf2129dbb1
Sai Htoo Lwin <saihtoolwin66@gmail.com>
2026-01-05 11:14:37 +0630

    add relationship

--- backend/app/Models/Role.php
+++ backend/app/Models/Role.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, "user_roles", "role_id", "user_id");
+    }
 }
